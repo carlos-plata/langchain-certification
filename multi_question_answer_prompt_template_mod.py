8516f73706a8fecdb6f46d6d7c4b58dd38ed3971
Carlos Plata <carlosplata@gmail.com>
2023-07-06 14:15:44 -0500

    added question(s)-answer(s) exercise

---
+++ multi_question_answer_prompt_template_mod.py
@@ -0,0 +1,33 @@
+from langchain import PromptTemplate
+from langchain import HuggingFaceHub, LLMChain
+
+
+multi_template = """Answer the following questions one at a time.
+Questions:
+{questions}
+Answers:
+"""
+long_prompt = PromptTemplate(template=multi_template, input_variables=["questions"])
+
+# initialize Hub LLM
+hub_llm = HuggingFaceHub(
+    repo_id='google/flan-t5-large',
+    model_kwargs={'temperature':0}
+)
+
+# create prompt template > LLM chain
+llm_chain = LLMChain(
+    prompt=long_prompt,
+    llm=hub_llm
+)
+
+qs_str = (
+    "What is the capital city of Portugal?\n" +
+    "What is the largest mammal on Earth?\n" +
+    "Which gas is most abundant in Earth's atmosphere?\n" +
+	"What color is a ripe mango?\n"
+)
+
+# print the response
+print(llm_chain.run(qs_str))
+
